f813c326825b0baa6f76979c198e0551e790e8d4
Michael <mreyna12@live.com>
2023-03-28 22:45:22 -0500

    add tests to confirm current_player resets to zero after getting to the last player. Check that winner will return a bool value

--- test_unittest.py
+++ test_unittest.py
@@ -18,8 +18,8 @@ class Test_Players_Class(unittest.TestCase):
         game = Game.Players()
         message = 'There is less or more than one person in player_container'
 
-        game.add_players('Michael')
-        game.add_players('Hillery')
+        game.add_players('Jacob')
+        game.add_players('Jessica')
         number_players =  game.how_many_players
 
         self.assertEqual(number_players, 2, message)
@@ -28,7 +28,7 @@ class Test_Players_Class(unittest.TestCase):
         game = Game.Players()
         message = 'The game should not be playable - check that only one player has been added.'
 
-        game.add_players('Michael')
+        game.add_players('Nicholas')
         expect_false = game.is_playable() 
 
         self.assertEqual(False, expect_false, message)   
@@ -37,8 +37,8 @@ class Test_Players_Class(unittest.TestCase):
         game = Game.Players()
         message = 'There are less than two or more than three players added to the players list'
 
-        game.add_players('Michael')
-        game.add_players('Hillery')
+        game.add_players('Priscilla')
+        game.add_players('Bailey')
         expect_true = game.is_playable()
 
         self.assertEqual(True, expect_true, message)
@@ -59,7 +59,7 @@ class Test_Check_Answers(unittest.TestCase):
           game = Game.StartGame()
           message = 'The purse amount should have gone from zero to one'
 
-          game.add_players('Michael')
+          game.add_players('Nicole')
           game.roll(8)
           game.actions_taken_for_correct_answer()
           expect_one = game.purses[0]
@@ -68,18 +68,43 @@ class Test_Check_Answers(unittest.TestCase):
 
       def test_go_to_next_player(self):
           game = Game.StartGame()
-          message = 'Test'
+          message = 'current_player was not increased by one'
 
-          game.add_players('Hillery')
-          game.add_players('Kira')
+          game.add_players('Stephanie')
+          game.add_players('Jaime')
           game.roll(6)
           
           game.increment_current_player_position()
           expect_kira = game.players[game.current_player]
 
-          self.assertEqual('Kira', expect_kira, message)
+          self.assertEqual('Jaime', expect_kira, message)
 
-          
+      def test_reset_current_player_to_zero(self):
+          game = Game.StartGame()
+          message = 'current_player was not set to 0'
+
+          game.add_players('Bill')
+          game.add_players('Jill')
+          game.roll(6)
+          game.increment_current_player_position()
+          game.roll(2)
+          game.increment_current_player_position()
+
+          expect_bill = game.players[game.current_player]
+
+          self.assertEqual('Bill', expect_bill, message)
+
+      def test_winner_returns_bool(self):
+          game = Game.StartGame()
+          message = 'The return value should be True or False'
+
+          game.add_players('Chris')
+          game.add_players('Samantha')
+          game.roll(3)
+
+          expect_bool = game.actions_taken_for_correct_answer()
+
+          self.assertIsInstance(expect_bool, bool, message)
         
 
         
