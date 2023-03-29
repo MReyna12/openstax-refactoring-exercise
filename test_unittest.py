253e4419fde0384edb0b5a685d8fbefdf5bf570a
Michael <mreyna12@live.com>
2023-03-28 21:26:35 -0500

    fix spacing and variable placement, as well as add purse unit test

--- test_unittest.py
+++ test_unittest.py
@@ -1,57 +1,71 @@
 import refactoring_exercise as Game
 import unittest
-import random
+
 
 class Test_Players_Class(unittest.TestCase):  
     
     def test_add_a_player(self):
-        player_information = Game.Players()
-        player_information.add_players('Michael')
-        player_information.add_players('Hillery')
-        player_container = player_information.players
+        game = Game.StartGame()
         message = 'Player was not added to the players list'
+        player_container = game.players
+
+        game.add_players('Michael')
+        game.add_players('Hillery')
+        
         self.assertIn('Michael', player_container, message)    
 
-    def test_how_many_players(self):
-        player_information = Game.Players()
-        player_information.add_players('Michael')
-        player_information.add_players('Hillery')
-        number_players =  player_information.how_many_players
+    def test_how_many_players_exist(self):
+        game = Game.Players()
         message = 'There is less or more than one person in player_container'
+
+        game.add_players('Michael')
+        game.add_players('Hillery')
+        number_players =  game.how_many_players
+
         self.assertEqual(number_players, 2, message)
 
     def test_one_player(self):
-        player_information = Game.Players()
-        player_information.add_players('Michael')
-        expectFalse = player_information.is_playable() 
-        self.assertEqual(False, expectFalse, 'The game should not be playable - check that only one player has been added.')   
+        game = Game.Players()
+        message = 'The game should not be playable - check that only one player has been added.'
+
+        game.add_players('Michael')
+        expect_false = game.is_playable() 
+
+        self.assertEqual(False, expect_false, message)   
 
     def test_correct_number_players(self):
-        player_information = Game.Players()
-        player_information.add_players('Michael')
-        player_information.add_players('Hillery')
-        expectTrue = player_information.is_playable()
-        self.assertEqual(True, expectTrue, 'There are less than two or more than three players added to the players list')
+        game = Game.Players()
+        message = 'There are less than two or more than three players added to the players list'
+
+        game.add_players('Michael')
+        game.add_players('Hillery')
+        expect_true = game.is_playable()
+
+        self.assertEqual(True, expect_true, message)
 
     def test_more_than_three_players(self):
-        player_information = Game.Players()
+        game = Game.Players()
+        message = 'The game should not be playable - check that more than three players were added.'
+  
         for i in range(5):
-          player_information.add_players('Michael %s' % i)
-        expectFalse = player_information.is_playable()
-        self.assertEqual(False, expectFalse, 'The game should not be playable because more than three players were added.') 
+          game.add_players('Michael %s' % i)
+        expect_false = game.is_playable()
 
-""" class Test_Check_Answers(unittest.TestCase):
-      player_information = Game.Players()
-      answers = Game.Check_Answer()
-      game = Game.StartGame()
+        self.assertEqual(False, expect_false, message) 
 
-      def test_correctly_answered(self):
-          self.player_information.add_players('Michael')
-          self.player_information.add_players('Jordan')
-          print('This is players list: %s' % self.player_information.players)
-          print('This is current_player value: %s' % self.player_information.current_player)
-          self.game.roll(8)
-          self.answers.was_correctly_answered() """
+class Test_Check_Answers(unittest.TestCase):
+
+      def test_purse_increase(self):
+          game = Game.StartGame()
+          message = 'The purse amount should have gone from zero to one'
+
+          game.add_players('Michael')
+          game.roll(8)
+          game.was_correctly_answered()
+          expect_one = game.purses[0]
+
+          self.assertEqual(1, expect_one, message)
+          
         
 
         
